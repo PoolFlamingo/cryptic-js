462cd8b5612c123e0508e56d65ddc1912b8572b0
Estigio97 <pablolopez280797@gmail.com>
2023-04-18 08:52:46 +0200

    Translate Ready

--- src/js/scripts/index.js
+++ src/js/scripts/index.js
@@ -1,4 +1,7 @@
 import { WindowButtons } from "../classes/elements/window-buttons.js";
 import { ObfuscatorForm } from "../classes/forms/obfuscator-form.js";
+import Translate from "../classes/global/translation.js";
+
+Translate.init();
 
 const windowButtons=new WindowButtons("window-buttons");
